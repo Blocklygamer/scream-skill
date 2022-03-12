de0f878611f927e7dfba6d654aa7220506a7ca6b
Stijn-student <stijn.rooryck@student.kuleuven.be>
2022-03-12 19:21:25 +0100

    Added scream sound effects

--- __init__.py
+++ __init__.py
@@ -1,13 +1,27 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.audioservice import AudioService
+from mycroft.audio import wait_while_speaking
+import random
+from os.path import dirname, join, walk
+
 
 
 class Scream(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        path = join(dirname(__file__),'screams')
+        self.playlist = []
+        for roots, dirs, files in walk(path):
+            for file in files:
+                self.playlist.append(join(path,file))
+
+    def initialize(self):
+        self.audioservice = AudioService(self.bus)
 
     @intent_file_handler('scream.intent')
     def handle_scream(self, message):
-        self.speak_dialog('scream')
+        scream = random.choice(self.playlist)
+        self.audioservice.play(scream)
 
 
 def create_skill():
